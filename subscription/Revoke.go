861714621e0004a80d505e17e6ed493c6bca064f
eric <mam09bog@studserv.uni-leipzig.de>
2016-10-06 00:24:02 +0200

    added sendMail when revoking, some improvements in mail templates

--- subscription/Revoke.go
+++ subscription/Revoke.go
@@ -14,6 +14,12 @@ func Revoke(db *sql.DB, verificationID, mailAddress string) error {
 		return err
 	}
 
+	var subscriptionID int
+	db.QueryRow(`
+		SELECT id
+		FROM subscriptions
+		WHERE mail_address = $1`, mailAddress).Scan(&subscriptionID)
+
 	_, err = db.Exec(`
 		DELETE FROM subscriptions
 		WHERE verification_id = $1 AND mail_address = $2`,
@@ -25,6 +31,7 @@ func Revoke(db *sql.DB, verificationID, mailAddress string) error {
 	}
 
 	deleteFile(verificationID)
+	sendMail(db, subscriptionID, "revoke")
 
 	// check if someone can move up
 	if SlotsAvailable(db) > 0 {
